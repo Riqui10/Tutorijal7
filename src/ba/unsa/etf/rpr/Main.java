a4804442d9d541fb0cdfd99f001b4691080acffc
Rijad Zejnilovic <rzejnilovi1@etf.unsa.ba>
2020-12-24 18:00:03 +0100

    Dodao sam klase i napravio izgled.

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package ba.unsa.etf.rpr;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +11,18 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        KorisniciModel model = new KorisniciModel();
+        Controller controller = new Controller(model);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        loader.setController(controller);
+        Parent root = loader.load();
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setMinHeight(400);
+        primaryStage.setMinWidth(600);
         primaryStage.show();
     }
 
